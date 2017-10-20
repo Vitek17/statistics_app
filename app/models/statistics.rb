4acf4bf5a66c340b6bb2f5b554d3db627c416f37
vitek <viktor.lebedev@phystech.edu>
2017-10-20 17:52:04 +0300

    new models

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -1,7 +1,6 @@
 class Statistics < ActiveRecord::Base
 
   def self.getAnswersStatistic(email, date)
-  	date += "%"
     MailsAnswersLog.joins(:user)
     .select("users.FIO")
     .where('mails_answers_logs.id_e = ? and DATE(mails_answers_logs.date_log) = ?', email, date)
