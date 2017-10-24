e031a16534052b02547e2d6ff9aae38a396282fe
vitek <viktor.lebedev@phystech.edu>
2017-10-24 20:21:05 +0300

    update model statistics

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -12,7 +12,7 @@ class Statistics
   def self.getAnswersStatistics(email, date)
     MailsAnswersLog.joins(:user)
     .select("users.FIO")
-    .where("mails_answers_logs.id_e = ? and date_trunc('day', mails_answers_logs.date_log) = ?", email, date)
+    .where("mails_answers_logs.id_e = ? and date_trunc('day', mails_answers_logs.date_log) = '2017-10-23' ", email)
     .group("users.FIO").count()
     .map{|x| x}
   end
@@ -20,7 +20,7 @@ class Statistics
   def self.getMovesStatistics(email, date)
   	MoveLog.joins(:user)
     .select("users.FIO")
-    .where("mails_move_log.id_e = ? and date_trunc('day', mails_move_log.date_log) = ?", email, date)
+    .where("mails_move_log.id_e = ? and date_trunc('day', mails_move_log.date_log) = '2017-10-23' ", email)
     .group("users.FIO").count()
     .map{|x| x}
   end
